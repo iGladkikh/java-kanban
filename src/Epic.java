4a64cbe406cdb43017709e0a9e66da4634f49343
Igor Gladkikh <kla2005@yandex.ru>
2024-02-09 16:13:49 +0300

    Ревью 1: 1. Тип поля subtasks - HashSet (хранятся id подзадач). 2. Метод вычисления поля Status перенесен в класс TaskManager.

--- src/Epic.java
+++ src/Epic.java
@@ -1,75 +1,28 @@
-import java.util.HashMap;
 import java.util.HashSet;
 
 public class Epic extends Task {
-    private final HashMap<Integer, Subtask> subtasks;
+    private final HashSet<Integer> subtasks;
 
     public Epic(String name, String description) {
         super(name, description);
-        this.subtasks = new HashMap<>();
+        this.subtasks = new HashSet<>();
     }
 
-    public Epic(String name, String description, HashMap<Integer, Subtask> tasks) {
-        super(name, description);
-        this.subtasks = tasks;
-        updateStatus();
-    }
-
-    private void updateStatus() {
-        if (subtasks.isEmpty()) {
-            setStatus(Status.NEW);
-            return;
-        }
-
-        HashSet<Status> statuses = new HashSet<>();
-        for (Subtask task : subtasks.values()) {
-            if (task.getStatus() == Status.NEW) {
-                statuses.add(Status.NEW);
-                break;
-            } else if (task.getStatus() == Status.IN_PROGRESS) {
-                statuses.add(Status.IN_PROGRESS);
-            }
-        }
-
-        if (statuses.contains(Status.NEW)) {
-            setStatus(Status.NEW);
-        } else if (statuses.contains(Status.IN_PROGRESS)) {
-            setStatus(Status.IN_PROGRESS);
-        } else {
-            setStatus(Status.DONE);
-        }
-    }
-
-    public void addSubtask(Subtask subtask) {
-        subtasks.putIfAbsent(subtask.getId(), subtask);
-        updateStatus();
-    }
-
-    public void addSubtask(Task task) {
-        Subtask subtask = new Subtask(task.getName(), task.getDescription(), task.getStatus(), getId());
-        subtasks.put(subtask.getId(), subtask);
-        updateStatus();
-    }
-
-    public void updateSubtask(Subtask subtask) {
-        int id = subtask.getId();
-        if (subtasks.containsKey(id)) {
-            subtasks.put(id, subtask);
-        }
-        updateStatus();
+    public void addSubtask(int id) {
+        subtasks.add(id);
     }
 
     public void clearSubtasks() {
         subtasks.clear();
-        setStatus(Status.NEW);
     }
 
     public void removeSubtask(int id) {
-        subtasks.remove(id);
-        updateStatus();
+        if (subtasks.contains(id)) {
+            subtasks.remove(id);
+        }
     }
 
-    public HashMap<Integer, Subtask> getSubtasks() {
+    public HashSet<Integer> getSubtasks() {
         return subtasks;
     }
 
@@ -80,7 +33,7 @@ public class Epic extends Task {
                 ", name='" + getName() + '\'' +
                 ", description='" + getDescription() + '\'' +
                 ", status=" + getStatus() +
-                ", subtasks=" + subtasks.size() +
+                ", subtaskIds=" + subtasks.size() +
                 '}';
     }
 }
