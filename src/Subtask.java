5e10d4fc1e51b9f0effc3a8cc8000e83a8223fa1
Igor Gladkikh <kla2005@yandex.ru>
2024-02-09 16:29:20 +0300

    Ревью 1: 1. Удалены лишние конструкторы.

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,16 +1,6 @@
 public class Subtask extends Task {
     private final int epicId;
 
-    public Subtask(String name, String description, int epicId) {
-        super(name, description);
-        this.epicId = epicId;
-    }
-
-    public Subtask(String name, String description, Status status, int epicId) {
-        super(name, description, status);
-        this.epicId = epicId;
-    }
-
     public Subtask(String name, String description, String status, int epicId) {
         super(name, description, status);
         this.epicId = epicId;
