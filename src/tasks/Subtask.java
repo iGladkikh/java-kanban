bc4440a2458eea226c44406cead705d45ef055ca
Igor Gladkikh <kla2005@yandex.ru>
2024-03-28 16:11:17 +0300

    Доработать функциональность по ТЗ спринта 7

--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -8,9 +8,20 @@ public class Subtask extends Task {
         this.epicId = epicId;
     }
 
+    public Subtask(int id, String name, String description, Status status, int epicId) {
+        super(id, name, description, status);
+        this.epicId = epicId;
+    }
+
     private Subtask(Subtask task) {
         super(task);
         this.epicId = task.getEpicId();
+    }
+
+    public static Subtask createFromString(String value, String fieldDelimiter) {
+        String[] data = value.split(fieldDelimiter);
+        return new Subtask(Integer.parseInt(data[0]), data[1], data[2],
+                Status.valueOf(data[3]), Integer.parseInt(data[4]));
     }
 
     public int getEpicId() {
@@ -28,7 +39,16 @@ public class Subtask extends Task {
                 '}';
     }
 
+    @Override
     public Subtask copy() {
         return new Subtask(this);
+    }
+
+    @Override
+    public String toSaveString(String delimiter) {
+        return String.join(delimiter, new String[] {
+                Type.SUBTASK.toString(), String.valueOf(getId()), getName(), getDescription(),
+                String.valueOf(getStatus()), String.valueOf(getEpicId())
+        });
     }
 }
